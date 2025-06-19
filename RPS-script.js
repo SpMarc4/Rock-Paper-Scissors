041f39ae9e7d434c6a245512a47d1d9be754f06c
Marc San <sanchezpe.marc@gmail.com>
2025-06-19 08:37:41 +0200

    Crear función que elige un valor al azar.
    Crear función que elige la decisión del PC de
    forma aleatoria, puede ser "rock", "paper" o "scissors".

--- RPS-script.js
+++ RPS-script.js
@@ -1,1 +1,25 @@
 console.log("Hello World")
+
+function getComputerChoice () {
+    // Función sin variables de entrada ya que el ordenador decide de forma aleatoria.
+    const randomNum = Math.random();
+    // Se crea una variable con un numero del 0 a 1 con Math.random.
+    // Se crea lógica condicional-
+    // Si valor entre 0 - 1/3 devuelve "rock".
+    if (randomNum >= 0 && randomNum < 1/3) {
+        return "rock";
+    }
+    // Si valor entre 1/3 - 2/3 devuelve "paper"
+    else if (randomNum >= 1/3 && randomNum < 2/3) {
+        return "paper";
+    }
+    // Si valor entre 2/3 - (Math.random) devuelve "paper"
+    else {
+        return "scissors"
+    };
+    
+
+}
+
+
+console.log(getComputerChoice())
